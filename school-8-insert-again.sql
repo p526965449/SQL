47321159353d1dd659aa631b937bca000f601b06
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-01-14 19:45:18 +0000

    Table Creation

---
+++ school-8-insert-again.sql
@@ -0,0 +1,6 @@
+--CREATE TABLE Praveen123 (ID Integer,Year Integer, Time Integer);
+--CREATE TABLE Praveen345 (ID Integer,Wins Integer);
+--INSERT INTO Praveen123 (ID,Year,Time) VALUES (1,2020,20),(2,2021,21),(3,2023,22);
+--SELECT * FROM Praveen123;
+--INSERT INTO Praveen345 (ID,Wins) VALUES (1,40),(2,42),(3,43);
+SELECT * FROM Praveen345;
